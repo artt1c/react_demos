2f0055edcb5e15f446f929f02a16f68b8043c3ff
artt1c <roma.myalo000@gmail.com>
2025-04-05 10:50:47 +0300

    part 6 HOOKS | useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect, useRef, useState} from 'react';
 import './App.css';
 
 const App: FC = ()=> {
+
+  const [number, setNumber] = useState<number>(0);
+
+
+  useEffect(() => {
+    console.log('work')
+  }, [number]);
+
   return (
     <>
+      <button onClick={() => {
+        setNumber(prevState => prevState + 1);
+      }}>change number</button>
 
     </>
   );
