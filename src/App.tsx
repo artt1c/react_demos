7dceb488b1caf5239cb88b6e35f66b3cdd23f1b7
artt1c <roma.myalo000@gmail.com>
2025-04-05 10:51:49 +0300

    part 6 HOOKS | useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import React, {FC} from 'react';
+import React, {FC, useState} from 'react';
 import './App.css';
 
 const App: FC = ()=> {
+
+  const [number, setNumber] = useState<number>(0);
+
   return (
     <>
+      <h2>{number}</h2>
+      <button onClick={() => {
+        setNumber(prevState => prevState + 1);
+      }}>change number</button>
 
     </>
   );
