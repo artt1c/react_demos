b69584ed7dbf9b8572ccce44b60dcf3f63f4ea31
artt1c <roma.myalo000@gmail.com>
2025-04-05 10:49:50 +0300

    part 6 HOOKS | useRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect, useRef, useState} from 'react';
 import './App.css';
 
 const App: FC = ()=> {
+
+  const [number, setNumber] = useState<number>(0);
+
+  let y = useRef<number>(0);
+
+  useEffect(() => {
+    console.log('work')
+  }, [number]);
+
   return (
     <>
+      <button onClick={() => {
+        setNumber(prevState => prevState + 1);
+      }}>change number</button>
 
+      <button onClick={() => {
+        y.current++;
+        console.log(y);
+      }}>change y</button>
     </>
   );
 }
