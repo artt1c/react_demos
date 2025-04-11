93c6c20945db50778f7eb4928fde556155193845
artt1c <roma.myalo000@gmail.com>
2025-04-11 15:14:34 +0300

    part 9 | redux toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import React, {FC} from 'react';
 import './App.css';
+import {decrement, increment, incrementByAmount, useAppDispatch, useAppSelector} from "./redux/store";
 
 const App: FC = ()=> {
-  return (
-    <>
 
-    </>
+  const counter1ValueState = useAppSelector(state => state.counter1SliceState.value);
+  const dispatch = useAppDispatch();
+
+  return (
+    <div>
+      <h2>{counter1ValueState}</h2>
+
+      <button onClick={()=>dispatch(increment())}>do increment</button>
+
+      <button onClick={()=>dispatch(decrement())}>do decrement</button>
+
+      <button onClick={()=>dispatch(incrementByAmount(10))}>do inc by amount 10</button>
+    </div>
   );
 }
 
